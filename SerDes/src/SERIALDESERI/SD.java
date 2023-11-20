fa69ba8e0e03bea4c54bdd883cdb20c620e9691d
Aditya Ravindra Pawar <144792472+adityapawar6020@users.noreply.github.com>
2023-11-19 23:59:36 -0800

    Add files via upload

---
+++ SerDes/src/SERIALDESERI/SD.java
@@ -0,0 +1,98 @@
+package SERIALDESERI;
+
+	import java.io.FileInputStream;
+	import java.io.FileOutputStream;
+	import java.io.IOException;
+	import java.io.ObjectInputStream;
+	import java.io.ObjectOutputStream;
+
+	class Demo implements java.io.Serializable{
+		
+
+			public int id;
+			public String name;
+
+			
+			public Demo(int id, String name)
+			{
+				this.id = id;
+				this.name = name;
+			}
+
+		}
+
+		class SD
+		{
+			public static void main(String[] args)
+			{ 
+				Demo object = new Demo(1, "Rajnish");
+			
+				
+			 
+				try
+				{ 
+					
+					FileOutputStream file = new FileOutputStream("f.txt");
+					ObjectOutputStream out = new ObjectOutputStream(file);
+					
+					
+					out.writeObject(object);
+					
+					out.close();
+					file.close();
+					
+					System.out.println("Object has been serialized");
+
+				}
+				
+				catch(IOException ex)
+				{
+					System.out.println("IOException is caught");
+				}
+
+
+				Demo object1 = null;
+
+				
+				try
+				{ 
+					
+					FileInputStream file = new FileInputStream("f.txt");
+					ObjectInputStream in = new ObjectInputStream(file);
+					
+					
+					object1 = (Demo)in.readObject();
+					
+					in.close();
+					file.close();
+					
+					System.out.println("Object has been deserialized ");
+					System.out.println("a = " + object1.id);
+					System.out.println("b = " + object1.name);
+				}
+				
+				catch(IOException ex)
+				{
+					System.out.println("IOException is caught");
+				}
+				
+				catch(ClassNotFoundException ex)
+				{
+					System.out.println("ClassNotFoundException is caught");
+				}
+
+			}
+		}
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+
